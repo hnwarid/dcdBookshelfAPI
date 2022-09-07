1d662685489f6bf0d644f5ef59f5b91c284b854d
Hamzah Nurwarid <hn.warid@gmail.com>
2022-09-07 17:34:52 +0700

    implement submission review suggestion

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Dicoding Bookshelf API  
 The  bookshelf API I submitted for 
 <a href="https://www.dicoding.com/academies/261">Belajar Membuat Aplikasi Back-End untuk Pemula</a> on 
-<a href="https://www.dicoding.com/">Dicoding<a/>.<br>
+<a href="https://www.dicoding.com/">Dicoding</a>.<br>
 I received <a href="https://www.dicoding.com/certificates/72ZDOL91LXYW
 ">this certificate</a> after completing the submission. 
--- src/server.js
+++ src/server.js
@@ -2,20 +2,20 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    server.route(routes);
-    
-    await server.start();
-    console.log(`BookshelfAPI server berjalan pada ${server.info.uri}`);
+  server.route(routes);
+
+  await server.start();
+  // console.log(`BookshelfAPI server berjalan pada ${server.info.uri}`);
 };
 
 init();
